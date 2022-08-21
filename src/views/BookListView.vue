fb1f05f998db7ca3ba9a0ebf143cbdca1c9d78e3
hengage <tunecarterkillz@gmail.com>
2022-08-21 02:04:58 +0100

    Feature: fetch book list data

--- src/views/BookListView.vue
+++ src/views/BookListView.vue
@@ -1,13 +1,31 @@
 <template>
     <div>
         <h3>We have these amazing books for you</h3> 
+        <ul>
+            <li v-for="book in bookList " :key="book.id">
+                <h3>{{ book.title }}</h3>
+                <p>{{ book.author }}</p>
+            </li>
+        </ul>
     </div>
 </template>
 
 <script>
     export default {
-    name: 'BookListView',
-    
-}
+        name: 'BookListView',
+        data() {
+            return {
+                bookList: []
+            
+            }
+        },
+        mounted() {
+            // fetch('http://127.0.0.1:8000/api/books/')
+            fetch('https://booksbackendapi.herokuapp.com/api/books/')
+            .then(res => res.json())
+            .then(data => this.bookList = data)
+            .catch(err => console.log(err.message))
+        }
+    }
 </script>
 
