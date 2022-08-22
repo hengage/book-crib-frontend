a770ff1542759c8df9f900e234754425314ca850
hengage <tunecarterkillz@gmail.com>
2022-08-22 01:48:17 +0100

    Feature: sign up featurr implemented
    post request to the api server for author sign up

--- src/components/AuthorForm.vue
+++ src/components/AuthorForm.vue
@@ -1,18 +1,19 @@
 <template>
     <form >
         <label for="">Email</label>
-        <input type="email" name="" >
+        <input type="email" name="" v-model="formData.email">
 
         <label for="firstName">First name</label>
-        <input type="text">
+        <input type="text" v-model="formData.first_name">
 
         <label for="lastName">Last name</label>
-        <input type="text">
+        <input type="text" v-model="formData.last_name">
 
         <label for="password">Password</label>
-        <input type="password" name="" >
+        <input type="password" name="" v-model="formData.password">
 
         <button type="submit">{{ submitAction }}</button>
+
     </form>
 </template>
 
@@ -20,6 +21,16 @@
     export default {
         props: {
             submitAction: {required: true}
-        }
+        },
+         data() {
+            return {
+               formData: {
+                    email: 'email',
+                    first_name: '',
+                    last_name: '',
+                    password: ''
+                }
+            };
+        },
     }
 </script>
--- src/views/AuthorsViews/SignUpView.vue
+++ src/views/AuthorsViews/SignUpView.vue
@@ -1,16 +1,59 @@
 <template>
     <div class="">
-        <AuthorForm submitAction='Create account'/>
-    </div>
+        <form @submit.prevent="handleSubmit">
+            <label for="">Email</label>
+            <input type="email" name="" v-model="formData.email">
+
+            <label for="firstName">First name</label>
+            <input type="text" v-model="formData.first_name">
+
+            <label for="lastName">Last name</label>
+            <input type="text" v-model="formData.last_name">
+
+            <label for="password">Password</label>
+            <input type="password" name="" v-model="formData.password">
+
+            <button type="submit">Create account</button>
+
+           
+        </form>
+        </div>
 </template>
 
 <script>
-    // import AuthorFormVue from "@/components/AuthorForm.vue";
-import AuthorForm from "@/components/AuthorForm.vue";
+    import axios from 'axios'
 
     export default {
         name: 'SignUpView',
-        components: { AuthorForm }
+        // components: { AuthorForm },
+        data() {
+            return {
+                formData: {
+                    email: '',
+                    first_name: '',
+                    last_name: '',
+                    password: ''
+                }
+               
+            }
+        },
+        methods: {
+            handleSubmit() {
+                 axios.post('http://127.0.0.1:8000/api/authors/', this.formData, {
+            })
+            .then(response => console.log(response))
+            .catch(err => console.log(err))
+            .catch((error) => console.log( error.response.request._response ) );
+                console.log('form submitted')
+                console.log(
+                    this.formData
+                )
+            }
+        },
+        mounted() {
+           
+        }
     }
+
 </script>
 
