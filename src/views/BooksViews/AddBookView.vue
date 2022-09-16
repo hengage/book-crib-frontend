8f44fe996ccea1e5484fb48900aa2d2c9bbd480b
hengage <tunecarterkillz@gmail.com>
2022-09-16 04:08:41 +0100

    Styles: css styles for addbookview

--- src/views/BooksViews/AddBookView.vue
+++ src/views/BooksViews/AddBookView.vue
@@ -1,16 +1,23 @@
 <template>
-    <div>
+    <div class="add-book">
         <h2>Add Your Book</h2>
 
-        <form @submit.prevent="handleSubmit">
-            <label for="title">title</label>
-            <input type="text" v-model="formData.title">
+        <form @submit.prevent="handleSubmit" class="book-form">
+            <div class="container">
+                <label for="title">Title</label>
+                <input type="text" v-model="formData.title">
+            </div>
 
-            <label for="isbn">isbn</label>
-            <input type="text" name="isbn" v-model="formData.isbn">
+            <div class="container">
+                <label for="isbn">ISBN</label>
+                <input type="text" name="isbn" v-model="formData.isbn">
+            </div>
 
-            <label for="author">Author</label>
-            <input type="number" name="author" v-model="formData.author">
+            <div class="container">
+
+                <label for="author">Author</label>
+                <input type="number" name="author" v-model="formData.author">
+            </div>
 
             <button type="submit">Save</button>
         </form>
@@ -34,11 +41,11 @@ export default {
     },
     methods: {
         handleSubmit() {
-            axios.post('https://booksbackendapi.herokuapp.com/api/books/', 
-            this.formData, {})
-                .then(response =>{
+            axios.post('https://booksbackendapi.herokuapp.com/api/books/',
+                this.formData, {})
+                .then(response => {
                     console.log(response)
-                    this.$router.push({name:'home'})
+                    this.$router.push({ name: 'home' })
                 })
                 .catch(err => {
                     console.log(err)
@@ -51,6 +58,34 @@ export default {
 
 }
 </script>
-<style lang="">
+<style >
+    .add-book {
+        margin-bottom: 3em;
+    }
+    .book-form .container {
+        margin: 1em 1em 2.5em 1em;
+    }
+
+    .book-form label {
+        display: block;
+        margin-bottom: 0.5em;
+    }
     
+    .book-form input{
+        width: 90%;
+        padding: 12px;
+        border: none;
+        border-radius: 0.5em;
+    }
+
+    .book-form button {
+        /* display: block; */
+        background-color: green;
+        color: white;
+        font-size: 1.6em;
+        border-radius: 0.4em;
+        margin-top: 1em;
+        padding: 0.44em 1.0em;
+      }
+
 </style>
